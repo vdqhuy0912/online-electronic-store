b42633984229b55ab0597bff228a04c64fc30205
VuQuangHuy <vdqhuy0912@gmail.com>
2024-05-24 11:21:41 +0700

    add

--- OnlineShop/OnlineShop/settings.py
+++ OnlineShop/OnlineShop/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Cart',
+    'Order',
+    'Coupons',
+    'users',
+    'products',
+    'HomePage'
 ]
 
 MIDDLEWARE = [
